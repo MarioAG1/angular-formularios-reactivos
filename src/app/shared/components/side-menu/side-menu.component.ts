9cf875b17ff956cd9db624df244ac92dd7f23418
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-12 17:50:32 +0100

    180. Estilos y creacion de las rutas

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+// Esto es para asegurase que siempre se caiga en una ruta y uno un undefined
+const reactiveItems = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  //Se hace por que hay mas de 1 menu
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((item) => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`,
+    }));
+
+  authMenu: MenuItem[] = [
+    {
+      title: 'Registro',
+      route: './auth',
+    },
+  ];
+
+  countryMenu: MenuItem[] = [
+    {
+      title: 'Paises',
+      route: './country',
+    },
+  ];
+}
