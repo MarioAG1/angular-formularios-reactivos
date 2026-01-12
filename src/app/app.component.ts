759641322c1df8e0947ef782dfae51480958ce19
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-12 16:50:10 +0100

    179 y 180 - Creacion del proyecto y puesta a punto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from './shared/components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, SideMenuComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class App {
   protected readonly title = signal('reactive-forms-app');
