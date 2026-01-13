70ebd03ffabbdb0d9af90fa18eceb50a2d1a7bf3
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-13 13:57:30 +0100

    182. FormBuilder

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -8,9 +8,19 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
+  // Esta no es la version mas optimo, From builder es mejor
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  private formBuilder = inject(FormBuilder);
+  //({}) {}, para pasar argumentos
+  myForm = this.formBuilder.group({
+    // El primer argumento es sincrono y el segundo asincrono [],[]
+    name: [''],
+    price: [0],
+    inStorage: [0],
   });
 }
