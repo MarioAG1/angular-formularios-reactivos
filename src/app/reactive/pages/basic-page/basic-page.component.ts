89c581aee2b7e6f8cefa5157004a0b9e12cc7fd1
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-13 18:42:48 +0100

    184. Mostrar errores en pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -23,10 +23,36 @@ export class BasicPageComponent {
 
   private formBuilder = inject(FormBuilder);
   //({}) {}, para pasar argumentos
-  myForm = this.formBuilder.group({
+  myForm: FormGroup = this.formBuilder.group({
     // El primer argumento es sincrono y el segundo asincrono [],[]
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
+
+  isValidField(fieldname: string): boolean | null {
+    return !!this.myForm.controls[fieldname].errors;
+  }
+
+  getFieldError(fieldname: string): string | null {
+    if (!this.myForm.controls[fieldname]) {
+      return null;
+    }
+
+    const errors = this.myForm.controls[fieldname].errors ?? {};
+
+    // Tiene que tenener el mismo texto tanto el caso como el return,
+    // no funciona y coerente a los tipo de errores en Angular
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo de ${errors['minlength'].requiredLength} caracteres.`;
+        case 'min':
+          return `Valor minimo de ${errors['min'].min}`;
+      }
+    }
+    return null;
+  }
 }
