3527a91d0efc45d4d8bfadb9056504147f7062d9
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-15 13:52:09 +0100

    196 - Switched, Checks y Radio Buttons

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,25 @@
+import { FormUtils } from './../../../utils/form-utils';
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent {}
+export class SwitchesPageComponent {
+  private formBuilder = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.formBuilder.group({
+    // Se le puede quitar el valor inicial M y no pondra ninguna opcion
+    gender: ['M', Validators.required],
+    wantNotifications: [true], //Al no poner Validators, esto sera opcional
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  onSumbit() {
+    this.myForm.markAllAsTouched();
+  }
+}
