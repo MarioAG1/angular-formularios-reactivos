3f7b9378f4fd503fa80a577567feaf88bef46359
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-16 19:03:28 +0100

    203. Primer elemento controlado - Continentes

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'country-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent {}
+export class CountryPageComponent {
+  formBuilder = inject(FormBuilder);
+  countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions);
+  countryByRegions = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm = this.formBuilder.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+}
