879d696aaf051fdd26639f4c41cd099329079ab0
MarioAG <marioalvarezgonzalez21@gmail.com>
2026-01-16 18:52:48 +0100

    202. Preparacion para peticios HTTP

---
+++ src/app/country/services/country.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
+
+@Injectable({ providedIn: 'root' })
+export class CountryService {
+  private baseUrl = 'https://restcountries.com/v3.1';
+  private http = inject(HttpClient);
+
+  private _regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  // Creamos el get, para que si llegan a modificar el arreglo no lo haga en _regions
+  get regions(): string[] {
+    return [...this._regions];
+  }
+
+  getCountriesByRegion(region: string): Observable<Country[]> {
+    // En vez de return [], pero dijimos de devolver un Observable, asi que ponemos of() para devolver un Observable vacio
+    if (!region) {
+      return of([]);
+    }
+
+    const url = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+    return this.http.get<Country[]>(url);
+  }
+
+  getCountryByAlphaCode(alphaCode: string): Observable<Country> {
+    const url = `${this.baseUrl}/region/${alphaCode}?fields=cca3,name,borders`;
+    return this.http.get<Country>(url);
+  }
+
+  getCountryBorderByCodes(border: string[]) {
+    //
+  }
+}
